6f7eee95757bed1bfbfc11e9a921734eabb841ad
RicardoPerdomo <ricardoperdomo0814@gmail.com>
2024-08-19 12:01:00 -0400

    project11

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const mainRouter = require("./routes/index");
+
+const app = express();
+const { PORT = 3001 } = process.env;
+
+mongoose.set("strictQuery", false);
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/wtwr_db")
+  .then(() => {
+    console.log("Connected to DB");
+  })
+  .catch((e) => console.error(e));
+
+app.use(express.json());
+app.use("/", mainRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
